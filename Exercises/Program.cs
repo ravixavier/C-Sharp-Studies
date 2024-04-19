4931cb45131ad19ab8d528c65e2f5278a30b004d
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-18 23:28:24 -0300

    Add project files.

---
+++ Exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1ercises
+{
+    internal class Program
+    {
+        /*
+         * Create & Initialise two ints
+         * Make a variable to work out the remainder
+         * Output remainder to ther screen
+         * Change the first int variable to another number
+              * and recalculate the remainder
+              * output new remainder to screen
+         */
+        static void Main(string[] args)
+        {
+            int num1 = 42348;
+            int num2 = 2;
+            var remainder = num1 % num2;
+
+            Console.WriteLine(remainder);
+
+            num1 = 12345;
+            remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+
+            Console.ReadLine();
+        }
+    }
+}
