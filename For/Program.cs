38e8b3b4f0307d2053b7f40c43513359c9ff376b
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-23 19:53:48 -0300

    While

--- For/Program.cs
+++ For/Program.cs
@@ -10,26 +10,38 @@ namespace For
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Hi!");
-            Console.WriteLine("Hi!");
-            Console.WriteLine("Hi!");
-            Console.WriteLine("Hi!");
-            */
 
-            Console.Write("How many time do you want to print 'hi'? ");
+            //Console.WriteLine("Hi!");
+            //Console.WriteLine("Hi!");
+            //Console.WriteLine("Hi!");
+            //Console.WriteLine("Hi!");
+
+
+            Console.WriteLine("What word do you want to repeat? ");
+            var message = Console.ReadLine();
+            Console.Write("How many times do you want to print it? ");
             int hiInput = Convert.ToInt32(Console.ReadLine());
-            
 
-            for (int i = 0; i < hiInput; i++)
+            if (hiInput <= 0)
             {
-                Console.WriteLine("hi");
+                Console.WriteLine("Sorry, Please enter a value above 0");
+            }
+            else
+            {
+
+                for (int i = 0; i < hiInput; i++)
+                {
+                    Console.WriteLine(message);
+                }
+
             }
 
-            for (int i = 0; i <= 10; i += 2)
-            {
-                Console.WriteLine(i);
-            }
+
+            //for (int i = 0; i <= 10; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
 
             Console.ReadLine();
         }
---
+++ While/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            int i = 0;
+
+            while (i < 10)
+            {
+                i++;
+                Console.WriteLine(i);
+            }*/
+
+            Console.Write("Enter the first number: ");
+            string numberAInput = Console.ReadLine();
+            int numberA = Convert.ToInt32(numberAInput);
+
+            Console.Write("Enter the second number: ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+
+            int answer = numberA * numberB;
+            int actualAnswer = 0;
+
+            
+           /* while (answer != actualAnswer)
+            {
+                Console.WriteLine("What's the value of " + numberA + " x " + numberB + "?");
+                string answerInput = Console.ReadLine();
+                actualAnswer = Convert.ToInt32(answerInput);
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Close but it was wrong! Try again!");
+                }
+            }*/
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("What's the value of " + numberA + " x " + numberB + "?");
+                string answerInput = Console.ReadLine();
+                actualAnswer = Convert.ToInt32(answerInput);
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Close but it was wrong! Try again!");
+                }
+            } while (answer != actualAnswer);
+
+            Console.WriteLine("Well Done!");
+
+            Console.ReadLine();
+        }
+    }
+}
