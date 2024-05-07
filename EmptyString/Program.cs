7b09a75aa582a39baeaf034f5b03d4a3d3f8f7e3
Ravi Xavier <ravixavier.dev@gmail.com>
2024-05-07 15:48:44 -0300

    Empty String

---
+++ EmptyString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                string name = "";
+                Console.WriteLine(name);
+
+                name = "Ravi";
+                Console.WriteLine(name);
+
+                Console.WriteLine(string.Concat(name, " S. C. Xavier"));
+                Console.WriteLine(name);
+            */
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            
+            if (name != string.Empty) //(name = string.Empty) é igual a (name = "";)
+                                      //a diferença é que string.Empty é mais consistente 
+                                      //em relação a escrita do código
+            {
+                Console.WriteLine($"Your names is {name}.");
+            }
+            else
+            {
+                Console.WriteLine("Name is empty.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
