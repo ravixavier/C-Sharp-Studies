6d4e2915835a290e4d034dbe43fd0d5d840d4cb0
Ravi Xavier <ravixavier.dev@gmail.com>
2024-05-08 11:59:17 -0300

    String Equals function

---
+++ StringEqualsFunction/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEqualsFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello";
+            string compare = "Hello";
+            
+            //if (message == compare)
+            /*
+                if (message.Equals(compare))
+                {
+                    Console.WriteLine("Same");
+                }
+                else
+                {
+                    Console.WriteLine("Diff");
+                }
+            */
+
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+
+            //if (name != "")
+            /*
+                if (!name.Equals(string.Empty))
+                {
+                    Console.WriteLine($"Your name is {name}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name output");
+                }
+            */
+
+            char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+            object newCompare = new string(chars);
+
+            if (message.Equals(newCompare))
+                Console.WriteLine("Same");
+            else
+                Console.WriteLine("Different");
+
+            Console.ReadLine();
+        }
+    }
+}
