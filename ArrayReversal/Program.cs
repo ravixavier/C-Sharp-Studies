622305791d194d374a4c92b14337f2b43477ce31
Ravi Xavier <ravixavier.dev@gmail.com>
2024-05-21 16:07:08 -0300

    Array Reversal

---
+++ ArrayReversal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                0, 1, 2, 3, 4, 5
+            };
+
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
