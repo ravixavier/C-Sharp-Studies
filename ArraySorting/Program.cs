3d0943dc10a0f5682d0f22fbbc9bae705d839ef0
Ravi Xavier <ravixavier.dev@gmail.com>
2024-05-20 21:29:23 -0300

    Array Sorting

---
+++ ArraySorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[]
+            {
+                9, 2, 0, 1, 5, 6, 5, 8, 9, 
+            };
+
+            Array.Sort(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
