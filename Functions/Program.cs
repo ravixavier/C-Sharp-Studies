bb72080d951d61a6b6b1b41429cf0ccba538a78b
Ravi Xavier <ravixavier.dev@gmail.com>
2024-06-04 22:25:46 -0300

    Functions

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            WelcomeMessage();
+
+            Console.ReadLine();
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome Ravi");
+        }
+    }
+}
