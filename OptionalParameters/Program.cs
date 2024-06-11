4d12b70d52710d8e28befdc54bde6389b0969707
Ravi Xavier <ravixavier.dev@gmail.com>
2024-06-11 18:31:14 -0300

    Optional Parameters

---
+++ OptionalParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(1);
+            Console.WriteLine(result);
+
+            /*
+             * nesse caso se não dermos um valor opcional ou default para o parametro em printName();
+             * será impossível chamar a função sem parametro algum.
+             * o valor default para um parametro string é uma string vazia.
+             */
+            printName();
+            //printName("Ravi");
+            
+            Console.ReadLine();
+        }
+
+        static void printName(string name = default)
+        {
+            Console.WriteLine($"Olá meu nome é {name}.");
+        }
+
+        static int Add(int a, int b = 27 /*ou vc pode dar o valor de 'default' que seria igual a 0*/)
+        {
+            return a + b;
+        }
+    }
+}
