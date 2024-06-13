f9f2fe27c4155955dabaeedd196fdd2f2c553f0b
Ravi Xavier <ravixavier.dev@gmail.com>
2024-06-13 17:56:11 -0300

    Named Parameters

---
+++ NamedParameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameInput = "Ravi";
+            int ageInput = 28;
+            string addressInput = "798, Something Road.";
+
+            PrintDeatails(age: ageInput, name: nameInput, address: addressInput);
+
+            /*
+             * apenas outra maneira de se utilizar os parametros em uma funcao
+             * ao invez de utilizar a funcao com os parametros exatamente na ordem
+             * especificada na funcao, vc pode 
+             */
+
+            Console.ReadLine();
+        }
+
+        static void PrintDeatails(string name, int age, string address)
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Address: {address}");
+        }
+    }
+}
