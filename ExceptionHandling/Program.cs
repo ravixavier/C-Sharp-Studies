ac251533ed050e6957df6cf323ba4e220acaa33f
Ravi Xavier <ravixavier.dev@gmail.com>
2024-07-02 21:29:03 -0300

    Try Catch

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.FormatException
+            //System.OverflowException
+
+            bool looping = true;
+
+            while (looping)
+            {
+                try
+                {
+                    Console.Write("Digite um número: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(num);
+
+                    looping = false;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Porfavor digite um numero menor que 2 biliões!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Porfavor apenas digite numeros!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ops! Algo de certo não está certo!");
+                }
+
+            }
+
+            Console.WriteLine("Tudo certo por aqui! \n Tchauzinho!");
+
+            Console.ReadLine();
+        }
+    }
+}
