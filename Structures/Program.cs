ccb5fde332507ffd02026f92b0d3b6a4cbe0425e
Ravi Xavier <ravixavier.dev@gmail.com>
2024-08-06 21:26:01 -0300

    Structures

---
+++ Structures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    internal class Program
+    {
+        struct Person
+        {
+            public string name;
+            public int age;
+            public int birthmonth;
+        }
+        static void Main(string[] args)
+        {
+            string name = "Ravi";
+            int age = 28;
+
+            Person person;
+
+            person.name = "Ravi";
+            person.age = 28;
+            person.birthmonth = 7;
+
+            Console.WriteLine($"{person.name} - {person.age} - {person.birthmonth}");
+
+            Console.ReadLine();
+        }
+    }
+}
