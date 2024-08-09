9551064bbf14f9d8e752100252c8e2f63ed2645b
Ravi Xavier <ravixavier.dev@gmail.com>
2024-08-09 17:06:48 -0300

    Structures part 2

--- Structures/Program.cs
+++ Structures/Program.cs
@@ -16,8 +16,9 @@ namespace Structures
         }
         static void Main(string[] args)
         {
-            string name = "Ravi";
+            /*string name = "Ravi";
             int age = 28;
+            int birthmonth = 7;
 
             Person person;
 
@@ -26,8 +27,57 @@ namespace Structures
             person.birthmonth = 7;
 
             Console.WriteLine($"{person.name} - {person.age} - {person.birthmonth}");
+            Console.WriteLine($"{name} - {age} - {birthmonth}");
+
+            string newName = "";
+            int newAge = 0;
+            int newBirthmonth = 0;
+
+            ReturnPerson(ref newName, ref newAge, ref newBirthmonth);
+            Console.WriteLine($"{newName} - {newAge} - {newBirthmonth}");*/
+
+            //Person person;
+            //person = ReturnPerson();
+            //aqui eu poderia ter escrito de uma forma mais abreviada, exemplo abaixo
+
+            Person person = ReturnPerson();
+
+            Console.WriteLine($"{person.name} - {person.age} - {person.birthmonth}");
 
             Console.ReadLine();
         }
+
+        static Person ReturnPerson()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your birthmonth: ");
+            int birthMonth = Convert.ToInt32(Console.ReadLine());
+
+            Person person;
+
+            person.name = name;
+            person.age = age;
+            person.birthmonth = birthMonth;
+
+            return person;
+        }
+
+        /*static void ReturnPerson(ref string name, ref int age, ref int birthMonth)
+        {
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your birthmonth: ");
+            birthMonth = Convert.ToInt32(Console.ReadLine());
+        }*/
+
     }
 }
